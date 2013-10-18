13a1c79d395171e66bb8beb79d0197cf40af34e7
Michael Nissim <info@israelunique.com>
2013-10-18 13:10:04 +0200

    Minor adjustments

--- app/helpers/outings_helper.rb
+++ app/helpers/outings_helper.rb
@@ -1,13 +1,18 @@
 module OutingsHelper
   def report_placeholder_text
-    s = "Puts each street and its block numbers on a new line:
-    <br><br>
-    Hertzl 45, 46, 50-58 <br>
+    s = "<b>Put each street and its block numbers on a new line:</b>
+    <br /><br />
+    Hertzl 45, 46, 50-58 <br />
     Sokolov 1-10, 15, 43
-    <br><br>
-    Can do even or odd numbers:
-    <br><br>
-    Weizmann 20-40 even, 40-50 odd"
+    <br /><br />
+    <b>Can do 'odd' and 'even' numbers as well:</b>
+    <br /><br />
+    Weizmann 20-40 even, 40-50 odd
+    <br /><br />
+    <b>Cannot do numbers with letters or slashes, like 21/8b:</b>
+    <br /><br />
+    Diezengof 15/8 <= not good <br />
+    Habonim 6b <= not good"
     
     s.html_safe
   end
