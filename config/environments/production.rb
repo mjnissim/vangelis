5d800634af2734d998a5f3a0d87e1ab11c38c971
Michael Nissim <info@israelunique.com>
2013-10-26 17:18:15 +0200

    precompile changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Vangelis::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.assets.precompile += %w[ *.css *.js ]
 end
