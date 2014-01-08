8a63bd8a332ea9d521a438a49b7a1507a5505bd4
Michael Nissim <info@israelunique.com>
2014-01-08 11:48:42 +0200

    Name Generator class for easy assignment names

---
+++ app/models/name_generator.rb
@@ -0,0 +1,15 @@
+class NameGenerator
+  def generate
+    rand_string( length: 2 ) + rand_number( digits: 2 ).to_s
+  end
+  
+  def rand_string length: 1
+    (1..length).map{(65 + rand(26)).chr}.join
+  end
+  
+  def rand_number digits: 2
+    min = ( 1.to_s + "0" * (digits - 1) ).to_i
+    max = ( 1.to_s + "0" * digits ).to_i - 1
+    rand(min..max)
+  end
+end
